ccec6737fab8de1359f6e3ba52eba9b074de6339
kydem <you@example.com>
2023-01-27 14:21:23 -0700

    Semi-Functional Rating Bars/ Div Alignment

--- client/components/Reviews/Rating.jsx
+++ client/components/Reviews/Rating.jsx
@@ -1,10 +1,12 @@
+import { findByLabelText } from "@testing-library/react";
 import React, { useState } from "react";
-import { FaRegBell, FaStar } from "react-icons/fa";
+import {  FaStar } from "react-icons/fa";
 import { Container, Radio, Rating } from "./RatingStyles";
-function Rate () {
+
+export function Rate () {
   const [rate, setRate] = useState(0);
   return (
-    <container>
+    <Container>
       {[...Array(5)].map((item, index) => {
         const givenRating = index + 1;
         return (
@@ -20,17 +22,47 @@ function Rate () {
               <FaStar
                 color={
                   givenRating < rate || givenRating === rate
-                    ? "000"
-                    : "rgb(255, 215, 0)"
+                    ? "rgb(255, 153, 0)"
+                    : "rgb(211, 211, 211)"
                 }
               />
             </Rating>
           </label>
         );
       })}
-    </container>
+    </Container>
   );
 };
 
-export default Rate;
+export const PercentageBar = ({bgcolor,progress,height}) => {
 
+  const Parentdiv = {
+    display: "flex",
+    height: height,
+    width: '15%',
+    backgroundColor: 'whitesmoke',
+    borderStyle: "solid",
+    borderColor: "rgb(211, 211, 211)",
+    borderWidth: 1,
+    borderRadius: 3,
+    margin: 7
+  }
+  
+  const Childdiv = {
+    height: '95%',
+    width: `${progress}%`,
+    backgroundColor: bgcolor,
+    borderStyle: "solid",
+    borderColor: "rgb(252, 97, 0)",
+    borderWidth: 1,
+    borderRadius: 1,
+    textAlign: 'right'
+  }
+  
+  return (
+    <div style={Parentdiv}>
+      <div style={Childdiv}>
+      </div>
+    </div>
+    )
+}
