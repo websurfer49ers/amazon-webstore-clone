89f2d6ab432ca2faa282bf3ec0178dfa9b025e09
kydem <you@example.com>
2023-01-27 15:28:18 -0700

    Working Reveiw Components; Styling is incomplete

--- client/components/Reviews/CustomerReviews.jsx
+++ client/components/Reviews/CustomerReviews.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { Rate, PercentageBar } from "./Rating.jsx"
+import { Container } from "./RatingStyles.jsx";
 
 
 function CustomerReviews(){
     return (
-        <div className="customerReviewDiv">
-            <div>
-                <Rate />
+        <span className="customerReviews">
+            <div className="customerReviewDiv">
+                <h2>Customer Reviews</h2>
+                <Rate className="calculatedStars"/>
                 <PercentageBar bgcolor="rgb(255, 153, 0)" progress='30'  height={15} />
                 <PercentageBar bgcolor="rgb(255, 153, 0)" progress='60'  height={15} />
                 <PercentageBar bgcolor="rgb(255, 153, 0)" progress='50'  height={15} />
                 <PercentageBar bgcolor="rgb(255, 153, 0)" progress='85'  height={15} />
                 <PercentageBar bgcolor="rgb(255, 153, 0)" progress='95'  height={15} />
             </div>
-        </div>
+        </span>
     )
 }
 
