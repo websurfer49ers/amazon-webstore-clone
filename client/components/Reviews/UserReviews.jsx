20a081787ed34fead6c0f96b28212fa68ffa744a
kydem <you@example.com>
2023-02-01 10:22:43 -0700

    wrappers and styiling added to review display

--- client/components/Reviews/UserReviews.jsx
+++ client/components/Reviews/UserReviews.jsx
@@ -6,14 +6,22 @@ export function UserReviews() {
     return (
         <div className="userReviewsMain">
             <div className="userReviewDisplay">
-                <h4>Top Reviews from the United States</h4>
+                <div className="topReviewsDiv">
+                    <span className="topReviews">Top Reviews from the United States</span>
+                </div>
                 <Container>
-                    <div>USER_IMAGE</div>
-                    <h6>Amazon Customer</h6>
+                    <div className="userImageWrapper">
+                        <img className="userImage" src="https://images-na.ssl-images-amazon.com/images/S/amazon-avatars-global/default._CR0,0,1024,1024_SX48_.png"></img>
+                    </div>
+                    <span className="userName">Amazon Customer</span>
                 </Container>
                 <Container>
-                    <Rate className="userStars"></Rate>
-                    <h6 className="reviewTitle">This is the best product</h6>
+                    <div className="userStars">
+                        <Rate></Rate>
+                    </div>
+                    <div className="reviewTitle">
+                        <span>This is the best product</span>
+                    </div>
                 </Container>
                 <p className="dateReviewed">Reviewed in the United States on March 2, 2019</p>
                 <p className="reviewContent">This product is great. This product is great. This product is great. This product is great. This product is great. This product is great. </p>
