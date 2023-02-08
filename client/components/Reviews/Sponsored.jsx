b50007ea2791c10de206def80e872fd6daefce72
Ty Sebesta <tnsebesta1998@gmail.com>
2023-02-07 16:47:53 -0800

    added comments to sponsored

--- client/components/Reviews/Sponsored.jsx
+++ client/components/Reviews/Sponsored.jsx
@@ -3,10 +3,18 @@ import { BiCaptions } from "react-icons/bi";
 import { FaCreativeCommonsSamplingPlus } from "react-icons/fa";
 import SponsoredItem from "./SponsoredItem.jsx";
 
+/**********************************************************************************
+The function SponsoredItem is the parent component for SponsoredItem.jsx
+
+The constant variable arrOfImages is an array of objects containing the product Id for each product in the database. In the return statement we use map to call SponsoredItem on every product and display the image and name from SponsoredItem.jsx. We also pass in productId and setProductId so that new products can be displayed on the page.
+
+*************************************************************************************/
+
 export function Sponsored(props) {
   const [sponsoredArr, setSponsoredArr] = useState([]);
   const [iteratingNum, setIteratingNum] = useState(0);
   const arrOfImages = props.productIdArr;
+  console.log(arrOfImages)
 
   return (
     <div style={{display:"flex"}}>
--- client/components/Reviews/SponsoredItem.jsx
+++ client/components/Reviews/SponsoredItem.jsx
@@ -1,5 +1,18 @@
 import React, { useEffect, useState } from "react";
 
+
+/**********************************************************************************
+The function SponsoredItem is called for every product that is in the database through Sponsored.jsx. 
+
+The fetch request returns the productName, imageurl and productId. The product name and the url for the image are saved as the useState variables productName and url.
+
+The function SponsoredHandler sets the productId to the productId of the sponsored item that is clicked so that the page will contain the new information about the product that was clicked.
+
+The div element with className "inner" is for the image and contains the styling for the image. The div element with className "text" is for the name of the product and contains all of the stylng for that as well. Both of these elements also have the SponsoredHandler function so that when they are clicked the page will change to the new product.
+
+I also added in "href="#"" on line 68 so that when you click on the sponsored item, it will take you up to the top of the page when the new item is displayed.
+
+*************************************************************************************/
 function SponsoredItem(props) {
   const [url, setUrl] = useState("");
   const [productName, setProductname] = useState("");
