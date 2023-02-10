f94e4384fe6c9a273f3079545cbe0fdc19986fa3
kydem <you@example.com>
2023-02-10 10:40:20 -0700

    Fixed a format bug

--- client/components/Reviews/UserReviews.jsx
+++ client/components/Reviews/UserReviews.jsx
@@ -15,8 +15,6 @@ export function UserReviews(props) {
       });
   }, [props.productId]);
 
-    const year = [2019, 2020, 2021, 2022, 2023]
-    const month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
   // console.log(returnedResults)
 
   return (
@@ -35,35 +33,6 @@ export function UserReviews(props) {
                     src="https://images-na.ssl-images-amazon.com/images/S/amazon-avatars-global/default._CR0,0,1024,1024_SX48_.png"
                   ></img>
                 </div>
-                {returnedResults.map((data) => {
-                    return (
-                        <div className="mapWrapper">
-                            <Container>
-                                <div className="userImageWrapper">
-                                    <img className="userImage" src="https://images-na.ssl-images-amazon.com/images/S/amazon-avatars-global/default._CR0,0,1024,1024_SX48_.png"></img>
-                                </div>
-                                <span className="userName">{data.firstname} {data.lastname}</span>
-                            </Container>
-                            <Container>
-                                <div className="userStars">
-                                    <Rated userStars={data.rating}/>
-                                </div>
-                                <div className="reviewTitle">
-                                    <span>{data.title}</span>
-                                </div>
-                            </Container>
-                            <p className="dateReviewed">Reviewed in the United States on {month[Math.ceil(Math.random() * 11)]} {Math.ceil(Math.random() * 28)}, {year[Math.ceil(Math.random() * 5)]}</p>
-                            <div className="contentWrapper">
-                                <p className="reviewContent">{data.content}</p>
-                            </div>
-                            <Container>
-                                <button className="helpfulButton">Helpful</button>
-                                <i className="textSperator" role="img"></i>
-                                <span className="reportAbuse">Report Abuse</span>
-                            </Container>
-                        </div>
-                    )
-                })}
                 <span className="userName">
                   {data.firstname} {data.lastname}
                 </span>
